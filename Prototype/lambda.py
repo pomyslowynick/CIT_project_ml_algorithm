ea06a4139b0da2a719e2e52a90f1d9e704160e2a
MattK159 <matthew.keatin2@mycit.ie>
2019-11-07 17:21:41 +0000

    Changed diabetes load path for AWS

--- Prototype/lambda.py
+++ Prototype/lambda.py
@@ -1,35 +1,53 @@
+import json
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.models import model_from_json
-import numpy as np
-from array import array
+import random
+import boto3
 
-	# get model and inputs from json
+def lambda_handler(event, payload):
+	# get model and inputs from payload
 
 	# call predict class with model and inputs
-
+	model = 1
+	inputs = [6,148,72,35,0,33.6,0.627,50]
+	
+	prediction = predictClass(model, inputs)
 	
 	
+	return {
+        'statusCode': 200,
+        'body': json.dumps(object_key)
+    }
 
 
 def predictClass(inmodel, inputs):
 	# load correct model
 	model = load_model(inmodel)
-	prediction = model.predict(np.array([inputs]))
+
+	# add inputs to np array
+	test = np.array([inputs])
+	# test - numpy array with vars
+	prediction = model.predict(test)
 
 	# add this to json and return prediction[0]*100
 	return int(prediction[0]*100)
 
 	# add patient details to database
+	
+	
+
 
 def load_model(model):
 	loaded_model = None
+	s3 = boto3.client('s3')
+
 	if(model == 1):
-		json_file = open('models/diab_model.json', 'r')
+		json_file = s3.get_object(Bucket='group-project-cit', Key='diab_model.json')
 		loaded_model_json = json_file.read()
 		json_file.close()
 		loaded_model = model_from_json(loaded_model_json)
-		loaded_model.load_weights("models/diab_model.h5")
+		weights = s3.get_object(Bucket='group-project-cit', Key='diab_model.h5')
+		loaded_model.load_weights(weights)
 		loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
 	elif(model == 2):
@@ -49,9 +67,4 @@ def load_model(model):
 		loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 	return loaded_model
 
-model = 1
-inputs = [6,148,72,35,0,33.6,0.627,50]
 
-prediction = predictClass(model, array)
-
-print(prediction)
