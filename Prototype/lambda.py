40c10eed9e50ef9df153d2c4648f997369d4380a
MattK159 <matthew.keating2@mycit.ie>
2019-11-08 20:30:00 +0000

    Updated lambda

--- Prototype/lambda.py
+++ Prototype/lambda.py
@@ -1,24 +1,9 @@
 import json
 from keras.models import Sequential
+from keras.models import model_from_json
 from keras.layers import Dense
+import numpy as np
 import random
-import boto3
-
-def lambda_handler(event, payload):
-	# get model and inputs from payload
-
-	# call predict class with model and inputs
-	model = 1
-	inputs = [6,148,72,35,0,33.6,0.627,50]
-	
-	prediction = predictClass(model, inputs)
-	
-	
-	return {
-        'statusCode': 200,
-        'body': json.dumps(object_key)
-    }
-
 
 def predictClass(inmodel, inputs):
 	# load correct model
@@ -39,16 +24,15 @@ def predictClass(inmodel, inputs):
 
 def load_model(model):
 	loaded_model = None
-	s3 = boto3.client('s3')
 
 	if(model == 1):
-		json_file = s3.get_object(Bucket='group-project-cit', Key='diab_model.json')
+		json_file = open('models/diab_model.json', 'r')
 		loaded_model_json = json_file.read()
 		json_file.close()
 		loaded_model = model_from_json(loaded_model_json)
-		weights = s3.get_object(Bucket='group-project-cit', Key='diab_model.h5')
-		loaded_model.load_weights(weights)
+		loaded_model.load_weights("models/diab_model.h5")
 		loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+		loaded_model.save("d_model")
 
 	elif(model == 2):
 		json_file = open('models/cancer_model.json', 'r')
@@ -67,4 +51,12 @@ def load_model(model):
 		loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 	return loaded_model
 
+
+# get model and inputs from payload
+
+# call predict class with model and inputs
+model = 1
+inputs = [6,148,72,35,0,33.6,0.627,50]
+
+prediction = predictClass(model, inputs)
 
